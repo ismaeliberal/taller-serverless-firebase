08e4ea02468188c7f47834aa9883817dffd21cc6
Ismael Liberal <ismael.liberal@biko2.com>
2020-02-04 13:34:15 +0100

    Version final

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import { Button, Input } from '@material-ui/core'
 import { Menu } from './components/Menu/Menu'
 import { ListadoPersonas } from './components/List/List'
+import { personaService } from './aplicacion/addPersona'
 
 export const App = () => {
   const [personas, setPersonas] = React.useState([])
@@ -10,12 +11,15 @@ export const App = () => {
   const addPersona = () => {
     const personasNew = [...personas]
     personasNew.push({ id: newPersona, nombre: newPersona })
+    personaService.add(newPersona)
     setPersonas(personasNew)
     setNewPersona('')
   }
 
   useEffect(() => {
-    const fetchData = async () => {}
+    const fetchData = async () => {
+      setPersonas(await personaService.getAll())
+    }
     fetchData()
   }, [])
 
